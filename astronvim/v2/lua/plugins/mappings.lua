82c903cdfcf76d9798c3fd99a428328d647910a9
VladMallory <vladmallory0321@gmail.com>
2026-02-26 17:44:06 +1000

    fix: изменение горячих клавиш. go test space + 3. Сохранение 2. rename space + 4

--- astronvim/v2/lua/plugins/mappings.lua
+++ astronvim/v2/lua/plugins/mappings.lua
@@ -4,16 +4,16 @@ return {
     mappings = {
       n = {
         ["<Leader>3"] = {
-          function() vim.lsp.buf.rename() end,
-          desc = "LSP Rename",
-        },
-        ["<Leader>4"] = {
           function()
             vim.cmd('TermExec cmd="go test ./..."')
           end,
           desc = "Run Go tests",
         },
-        ["5"] = {
+        ["<Leader>4"] = {
+          function() vim.lsp.buf.rename() end,
+          desc = "LSP Rename",
+        },
+        ["2"] = {
           function()
             vim.cmd("write")
           end,
