c88a49719371ef63254079274b53a2ea14536f85
Коваленко Владислав Анатольевич <vladmallory0321@gmail.com>
2026-04-08 17:20:39 +1000

    feat: добавлена стрелочка при переносе строк которые переходят границу экрана. И адекватный скрол с переносом экрана

--- astronvim/v3/lua/plugins/editor.lua
+++ astronvim/v3/lua/plugins/editor.lua
@@ -6,6 +6,12 @@ return {
         opt = {
           number = true,
           relativenumber = false,
+          scrolloff = 5,
+          wrap = true,
+          linebreak = true,
+          breakindent = true,
+          breakindentopt = "shift:0,sbr",
+          showbreak = "↪  ",
         },
       },
     },
