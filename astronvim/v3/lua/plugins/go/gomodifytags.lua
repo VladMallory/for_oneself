de5d4ec472e598555554efe2579f15e7ced990fd
Vlad <vlad@Mac-mini-Vlad.local>
2026-05-08 16:04:27 +1000

    feat: добавлена возможность быстро заполнять структуру json полями

---
+++ astronvim/v3/lua/plugins/go/gomodifytags.lua
@@ -0,0 +1,31 @@
+local go_bin = vim.fn.expand "$HOME/go/bin"
+
+return {
+  {
+    "simondrake/gomodifytags",
+    ft = "go",
+    init = function()
+      if vim.fn.executable "gomodifytags" == 0 and vim.fn.isdirectory(go_bin) == 1 then
+        vim.env.PATH = go_bin .. ":" .. vim.env.PATH
+      end
+    end,
+    opts = {
+      parse = { enabled = true, separator = "--" },
+    },
+    config = function(_, opts)
+      require("gomodifytags").setup(opts)
+
+      local function command_opts(cmd)
+        if cmd.args:find("%s%-%-%s") then return cmd.args end
+      end
+
+      vim.api.nvim_create_user_command("GoAddTags", function(cmd)
+        require("gomodifytags").GoAddTags(cmd.fargs[1], command_opts(cmd))
+      end, { nargs = "+" })
+
+      vim.api.nvim_create_user_command("GoRemoveTags", function(cmd)
+        require("gomodifytags").GoRemoveTags(cmd.fargs[1], command_opts(cmd))
+      end, { nargs = "+" })
+    end,
+  },
+}
--- astronvim/v3/lua/plugins/go/init.lua
+++ astronvim/v3/lua/plugins/go/init.lua
@@ -2,4 +2,5 @@ return {
   { import = "plugins.go.gopls" },
   { import = "plugins.go.none-ls" },
   { import = "plugins.go.mason" },
+  { import = "plugins.go.gomodifytags" },
 }
