875f39171edc9d7793b318f67f574934718660a7
VladMallory <vladmallory0321@gmail.com>
2026-06-07 01:51:07 +1000

    fix: убраны повторные отображения линтера

--- astronvim/v3/lua/plugins/go/none-ls.lua
+++ astronvim/v3/lua/plugins/go/none-ls.lua
@@ -1,7 +1,12 @@
+local none_ls_configured = false
+
 return {
   {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
+      if none_ls_configured then return end
+      none_ls_configured = true
+
       local null_ls = require "null-ls"
 
       opts.sources = require("astrocore").list_insert_unique(opts.sources or {}, {
