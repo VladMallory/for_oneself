33569e2f02d96be45579c10aa34d000b10b05621
VladMallory <vladmallory0321@gmail.com>
2026-06-27 20:42:45 +1000

    feat: добавлен steam

--- arch/packages/packages.sh
+++ arch/packages/packages.sh
@@ -10,7 +10,7 @@ sudo pacman -S --needed --noconfirm \
     btop fastfetch ffmpeg rsync tree wget unzip firefox \
     thunar qalculate-gtk baobab qbittorrent flatpak \
     telegram-desktop vlc obs-studio easyeffects element-desktop \
-    signal-desktop bitwarden
+    signal-desktop bitwarden steam
 
 log "=== AUR пакеты ==="
 if ! command -v flclash &> /dev/null; then
