9b0cec1629839cb3113966d62f0bea069aa103a3
VladMallory <vladmallory0321@gmail.com>
2026-06-28 12:03:12 +1000

    locale: add ru_RU.UTF-8 alongside en_US.UTF-8

--- arch/packages/locale.sh
+++ arch/packages/locale.sh
@@ -5,8 +5,11 @@ set -e
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source "$DIR/common.sh"
 
-log "=== Включение en_US.UTF-8 в locale.gen ==="
-sudo sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+log "=== Включение en_US.UTF-8 и ru_RU.UTF-8 в locale.gen ==="
+sudo sed -i \
+  -e 's/^#en_US.UTF-8/en_US.UTF-8/' \
+  -e 's/^#ru_RU.UTF-8/ru_RU.UTF-8/' \
+  /etc/locale.gen
 sudo locale-gen
 
-log "Локаль en_US.UTF-8 включена"
+log "Локали en_US.UTF-8 и ru_RU.UTF-8 включены"
