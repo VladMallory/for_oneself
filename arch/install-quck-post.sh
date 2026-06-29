c20bc6403cbcef8b1e7fa5a32db9ad333b598958
VladMallory <vladmallory0321@gmail.com>
2026-06-29 23:51:51 +1000

    fix: не запускался post

--- arch/install-quck-post.sh
+++ arch/install-quck-post.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-if ! command -v curl &>/dev/null; then
-    sudo pacman -Sy --noconfirm curl
-fi
+pacman -Sy --noconfirm curl git
+
 git clone --depth 1 https://github.com/VladMallory/for_oneself.git /tmp/for_oneself
 cd /tmp/for_oneself/arch
 bash install.sh
