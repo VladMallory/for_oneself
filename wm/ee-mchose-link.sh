e01daa4a9e717a712c6c1593c2d85271a35dcf64
vladmallory <vladmallory0321@gmail.com>
2026-07-04 15:39:36 +1000

    easyeffects: input preset Mic Boost with gate/compressor/limiter; auto-set default source

---
+++ wm/ee-mchose-link.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SINK="alsa_output.usb-C-Media_Electronics_Inc_MCHOSE_V9_PRO_0123456789AB-01.analog-stereo"
+sleep 3
+pw-link easyeffects_sink:monitor_FL "$SINK":playback_FL 2>/dev/null
+pw-link easyeffects_sink:monitor_FR "$SINK":playback_FR 2>/dev/null
+pactl set-default-sink easyeffects_sink 2>/dev/null
+pactl set-default-source easyeffects_source 2>/dev/null
