b1accbb88295c347b8767d306cde2915a1dac3b8
VladMallory <vladmallory0321@gmail.com>
2026-07-20 19:15:14 +1000

    refactor: легкий рефактор

--- astronvim/v3/lua/polish.lua
+++ astronvim/v3/lua/polish.lua
@@ -1,4 +1,4 @@
--- Отключаем format_on_save для Go (форматируем через BufWritePre в gopls.lua)
+-- Отключаем format_on_save для Go (форматируем через BufWritePre в format.lua)
 local astrolsp = require("astrolsp")
 if astrolsp and astrolsp.config then
   astrolsp.config.formatting.format_on_save.ignore_filetypes = { "go" }
