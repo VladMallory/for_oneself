61c43b938e6265777d7b1530d0ebb44827610c5e
VladMallory <vladmallory0321@gmail.com>
2026-07-21 22:18:33 +1000

    init lia

--- astronvim/v3/lua/plugins/go/init.lua
+++ astronvim/v3/lua/plugins/go/init.lua
@@ -1,5 +1,6 @@
 return {
-  { import = "plugins.go.gopls" },
+  { import = "plugins.go.lsp" },
+  { import = "plugins.go.format" },
   { import = "plugins.go.none-ls" },
   { import = "plugins.go.mason" },
   { import = "plugins.go.gomodifytags" },
