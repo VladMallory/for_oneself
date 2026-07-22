b6ff9e46360a4f76b09858d1c3691868290dccb4
VladMallory <vladmallory0321@gmail.com>
2026-07-22 16:34:37 +1000

    убрал NOPASSWD — sudo спросит пароль как обычно

--- arch/install-quck-post.sh
+++ arch/install-quck-post.sh
@@ -11,10 +11,6 @@ git clone --depth 1 https://github.com/VladMallory/for_oneself.git /tmp/for_ones
 if [ -n "$BUILD_USER" ]; then
     chown -R "$BUILD_USER" /tmp/for_oneself
 
-    echo "$BUILD_USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/99-temp-install
-    chmod 440 /etc/sudoers.d/99-temp-install
-    trap 'rm -f /etc/sudoers.d/99-temp-install' EXIT
-
     cd /tmp/for_oneself/arch
     sudo -u "$BUILD_USER" -H bash install.sh
 else
